db3deff9c7fc8dea8df865fcbb025784747fe67c
shynome <shynome@gmail.com>
2017-07-16 17:52:37 +0800

    add: redirect to /

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
 export const router = Router()
-
+//redirect to /
+router.use((req,res,next)=>{
+  if(
+        Object.keys(req.query).length
+    ||  /\/$/.test(req.originalUrl)
+  ){
+    next()
+    return
+  }
+  res.redirect(req.originalUrl+'/')
+})
+//manger
+import { server as manager } from "./manager";
+router.use('/manager',manager)
+//server
 import { server as shylog } from "./shylog";
 router.use('/',shylog)
