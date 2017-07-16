ca8a97f74797284e4d37446889014291510d0a78
shynome <shynome@gmail.com>
2017-07-17 00:53:20 +0800

    optimization : in production remove sourcemap options

--- ts.js
+++ ts.js
@@ -1,5 +1,5 @@
-require('ts-node').register({ fast:true, project:__dirname })
 const dev = global.dev = exports.dev = !(/production/i.test(process.env.NODE_ENV))
+require('ts-node').register({ fast:true, project:__dirname, sourceMap:dev })
 if(dev){
   require('require-dynamic-exec').watch(__dirname,true)
 }
