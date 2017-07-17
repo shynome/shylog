0cc4d0709f2db9c9a57da2ed1fdf2d7b7029ead3
shynome <shynome@gmail.com>
2017-07-17 22:37:28 +0800

    using glamorous

--- shylog/views/index.tsx
+++ shylog/views/index.tsx
@@ -1,5 +1,19 @@
 import React = require('react');
+import glamorous from 'glamorous'
+const Styled = glamorous.div({
+  '& h1':{
+    'color':'red',
+    '&:hover':{
+      'color':'yellow',
+    },
+    '@media screen and (min-width:1200px)':{
+      '&':{
+        'color':'blue'
+      }
+    }
+  }
+})
 export default ()=>
-<h1>
-  welcome to shylog , it's building
-</h1>
+<Styled>
+  <h1>welcome to shylog , it's building</h1>
+</Styled>
