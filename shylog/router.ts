8819d5ab3deeff7c41c1a48b54245798d294fb1c
shynome <shynome@gmail.com>
2017-07-17 22:47:07 +0800

    set lang and index title

--- shylog/router.ts
+++ shylog/router.ts
@@ -1,4 +1,11 @@
 import { Router } from "express";
 export const router = Router()
 
-router.get('/',(req,res)=>res.render('./views/index.tsx'))
+router.use((req,res,next)=>{
+  res.locals.lang = 'zh-hms-cn'
+  next()
+})
+
+router.get('/',(req,res)=>{
+  res.render('./views/index.tsx',{ title:'welcome to shylog' })
+})
