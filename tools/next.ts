064990243d90c3f68e950078f9891a04f00553c3
shynome <shynome@gmail.com>
2017-07-18 23:52:00 +0800

    添加了全局函数 next_auto

---
+++ tools/next.ts
@@ -0,0 +1,18 @@
+import { RequestHandler,Request,Response } from "express";
+export type next = (handler:RequestHandler)=>RequestHandler
+export const next:next = (global as any).next_auto = 
+(handler:RequestHandler):RequestHandler=>
+(req,res,next)=>
+new Promise(async (resolve,reject)=>{
+  let next_wrapper = (error?:any)=>(error?resolve:reject)(error)
+  await handler(req,res,next_wrapper)
+  next_wrapper()
+})
+.then(
+  ()=>next(),
+  next
+)
+
+declare global {
+  var next_auto:next
+}
