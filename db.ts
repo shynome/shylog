ef4c7423695fa7d808d373d2747bdbee2664a122
shynome <shynome@gmail.com>
2017-07-18 23:59:00 +0800

    连接线上数据库

--- db.ts
+++ db.ts
@@ -1,5 +1,6 @@
 import { MongoClient,Db,Collection } from "mongodb";
-export const db_client = MongoClient.connect('mongodb://127.0.0.1')
+export let mongodb_url =`mongodb://root:zxcvgf@web-shard-00-00-ehvyy.mongodb.net:27017,web-shard-00-01-ehvyy.mongodb.net:27017,web-shard-00-02-ehvyy.mongodb.net:27017/web?ssl=true&replicaSet=web-shard-0&authSource=admin`
+export const db_client = MongoClient.connect(mongodb_url)
 import { Router } from "express";
 export const router = Router()
 declare global {
