31994744a37661fb30eb2bcf3016680fea27a3d6
shynome <shynome@gmail.com>
2017-08-21 22:26:46 +0800

    restart

--- shylog/views/index.tsx
+++ shylog/views/index.tsx
@@ -1,22 +1,5 @@
 import React = require('react');
-import glamorous from 'glamorous'
-const Styled = glamorous.div({
-  position:'absolute', textAlign:'center',
-  top:0, left: 0, width:'100%', height:'100%',
-  '& .table':{
-    display:'table', height:'100%', width:'100%',
-    '& .middle':{
-      display:'table-cell', verticalAlign:'middle',
-      fontSize:'15vw',  paddingBottom:'10%',
-    }
-  },
-  '& span':{
-    transition:'all 3s',
-  }
-})
 import { App } from './App'
-export const getRandomColorNumber = ()=>Math.floor(Math.random()*256)
-export const getRandomColor = ()=>Array.from(' '.repeat(3)).map(getRandomColorNumber)
 export default class Index extends React.Component<void,any>{
   words = 'shynome'
   state = { next:0 }
@@ -27,17 +10,9 @@ export default class Index extends React.Component<void,any>{
   render(){
     this.state.next ? setTimeout(this.next,3000) : this.next()
     return (
-    <Styled>
-      <div className="table">
-        <div className="middle">
-          {
-          Array.from(this.words).map((word,index)=>(
-          <span key={index} style={{ color:`rgb(${getRandomColor().join(',')})` }}>{word}</span>
-          ))
-          }
-        </div>
-      </div>
-    </Styled>
+    <div>
+      shynome
+    </div>
     )
   }
 }
