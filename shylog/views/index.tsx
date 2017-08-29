0dd4dd16586ab200921b540e1df23e26712e33e2
shynome <shynome@gmail.com>
2017-08-29 23:34:23 +0800

    for simple

--- shylog/views/index.tsx
+++ shylog/views/index.tsx
@@ -1,13 +1,2 @@
 import React = require('react');
-import { App } from './App'
-import glamorous,{  } from 'glamorous';
-const Styled = glamorous.div({
-  
-})
-export const View = <App>
-  <Styled>
-    <div className="tabs">
-
-    </div>
-  </Styled>
-</App>
+export const View = <div>shynome</div>
