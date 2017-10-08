b2d84ed5613ae79c2ec47ea5c6f6181591ad4526
shynome <shynome@gmail.com>
2017-10-08 19:23:20 +0800

    archive

--- user/views/login.tsx
+++ user/views/login.tsx
@@ -3,29 +3,33 @@ import *as React from 'react';
 import { Tab, Button, Input, Form } from 'semantic-ui-react';
 const { Pane } = Tab
 const { Field } = Form
+const enum ActionType { login, sign }
 export default class extends React.Component {
-  login = ()=>(<Pane>
-    <Form method="post" action="./login">
-      <Field><Input iconPosition="left" icon="user" placeholder="username" name="username"></Input></Field>
-      <Field><Input iconPosition="left" icon="lock" placeholder="password" type="password" name="password"></Input></Field>
-      <Button type="submit">login in</Button>
-    </Form>
-  </Pane>)
-  sign = ()=>(<Pane>
-    <Form method="post" action="./sign">
-      <Field><Input iconPosition="left" icon="user" placeholder="username" name="username"></Input></Field>
-      <Field><Input iconPosition="left" icon="lock" placeholder="password" type="password" name="password"></Input></Field>
-      <Field><Input iconPosition="left" icon="lock" placeholder="password confirm" type="password" name="password_confirm"></Input></Field>
-      <Button type="submit">sign up</Button>
-    </Form>
-  </Pane>)
-  panes = [
-    { menuItem:'login in', render:this.login },
-    { menuItem:'sign up', render:this.sign }
-  ]
+  login = (e)=>{
+    debugger
+  }
+  sign = ()=>{
+
+  }
   render(){
     return <div style={{ maxWidth:400, margin:'40px auto' }}>
-      <Tab panes={this.panes}></Tab>
+      <Tab panes={[
+        { menuItem:'login in', render:()=>(<Pane>
+          <Form method="post" action="./login" onSubmit={this.login}>
+            <Field><Input iconPosition="left" icon="user" placeholder="username" name="username"></Input></Field>
+            <Field><Input iconPosition="left" icon="lock" placeholder="password" type="password" name="password"></Input></Field>
+            <Button type="submit">login in</Button>
+          </Form>
+        </Pane>)},
+        { menuItem: 'sign up', render:()=>(<Pane>
+          <Form method="post" action="./sign" onSubmit={this.sign}>
+            <Field><Input iconPosition="left" icon="user" placeholder="username" name="username"></Input></Field>
+            <Field><Input iconPosition="left" icon="lock" placeholder="password" type="password" name="password"></Input></Field>
+            <Field><Input iconPosition="left" icon="lock" placeholder="password confirm" type="password" name="password_confirm"></Input></Field>
+            <Button type="submit">sign up</Button>
+          </Form>
+        </Pane>)}
+      ]}></Tab>
     </div>
   }
 }
